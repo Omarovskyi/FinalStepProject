a80d4dd2298f8b9c5effc15dd2a11f1049eaae87
Omarovskyi <Omarovskiu@gmail.com>
2025-07-14 23:59:13 +0300

    Changes port main

--- app/main.py
+++ app/main.py
@@ -8,6 +8,3 @@ def get_host_ip():
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
     return f"<strong>Host IP: </strong>{ip}\n"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5500)
